c45fb74c592f43beb500512129d93146f27cb08e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-29 12:27:37 +0000

    Adding tarfile member sanitization to extractall()

--- galaxy/code/tool_PartsGenie.py
+++ galaxy/code/tool_PartsGenie.py
@@ -48,7 +48,26 @@ if __name__ == "__main__":
         client = rpTool.PartsGenieClient(params.server_url)
         with tempfile.TemporaryDirectory() as tmpInputFolder:
             with tarfile.open(params.input) as rf:
-                rf.extractall(tmpInputFolder)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(rf, tmpInputFolder)
             in_files = glob.glob(tmpInputFolder+'/*')
             #write the tar
             with tarfile.open(params.output, mode='w:gz') as ot:
